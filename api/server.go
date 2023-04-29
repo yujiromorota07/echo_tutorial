72d21bef0254cbbc4f4713e4101e1a9492db3d3e
yujiromorota07 <yujiromorota07@gmail.com>
2023-04-29 18:45:42 +0900

    mysql環境docker化

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"api/di"
 	todo "api/generated/todo"
+	sql "database/sql"
+	"log"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
@@ -13,6 +15,12 @@ func main() {
 	todoController := di.IntializeTodoController()
 	todoAPI := e.Group("")
 	todo.RegisterHandlers(todoAPI, todoController)
+
+	db, err := sql.Open("mysql", "echo:echo@tcp(127.0.0.1:3306)/echodev")
+	if err != nil {
+		log.Fatalf("main sql.Open error err:%v", err)
+	}
+	defer db.Close()
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
