0c76ae1c7a98cf4fa3af519868dc8e906fe5f619
yujiromorota07 <yujiromorota07@gmail.com>
2023-04-29 14:25:46 +0900

    apiの環境をdocker化する

--- api/go.mod
+++ api/go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require github.com/labstack/echo/v4 v4.10.2
 
+require github.com/go-sql-driver/mysql v1.7.1
+
 require (
 	github.com/google/wire v0.5.0
 	github.com/labstack/gommon v0.4.0 // indirect
--- api/server.go
+++ api/server.go
@@ -4,12 +4,12 @@ import (
 	"api/di"
 	todo "api/generated/todo"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
-
 	todoController := di.IntializeTodoController()
 	todoAPI := e.Group("")
 	todo.RegisterHandlers(todoAPI, todoController)
