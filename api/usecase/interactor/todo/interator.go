45fa3d0f67c29aef6e9f6d6403ec4fec9e3930d4
yujiromorota07 <yujiromorota07@gmail.com>
2023-05-03 15:14:53 +0900

    トランザクション実装、ユースケースに組み込み、todo編集機能フロー変更

--- api/usecase/interactor/todo/interator.go
+++ api/usecase/interactor/todo/interator.go
@@ -8,11 +8,13 @@ import (
 )
 
 type TodoUsecase struct {
+	transaction    repository.TransactionManager
 	todoRepository repository.TodoRepository
 }
 
-func NewTodoUsecase(todoRepository repository.TodoRepository) inputport.Todo {
+func NewTodoUsecase(transaction repository.TransactionManager, todoRepository repository.TodoRepository) inputport.Todo {
 	return &TodoUsecase{
+		transaction:    transaction,
 		todoRepository: todoRepository,
 	}
 }
@@ -39,30 +41,37 @@ func (usecase TodoUsecase) GetTodo(e entity.TodoID) (entity.Todo, error) {
 
 func (usecase TodoUsecase) CreateTodo(e entity.Todo) (entity.Todo, error) {
 	ctx := context.Background()
-	todo, err := usecase.todoRepository.CreateTodo(ctx, e)
+	v, err := usecase.transaction.Do(ctx, func(ctx context.Context) (interface{}, error) {
+		return usecase.todoRepository.CreateTodo(ctx, e)
+	})
+
 	if err != nil {
 		return entity.Todo{}, err
 	}
 
+	todo := v.(entity.Todo)
 	return todo, nil
 }
 
 func (usecase TodoUsecase) UpdateTodo(e entity.Todo) (entity.Todo, error) {
 	ctx := context.Background()
-	err := usecase.todoRepository.UpdateTodo(ctx, e)
-	if err != nil {
-		return entity.Todo{}, err
-	}
+	_, err := usecase.transaction.Do(ctx, func(ctx context.Context) (interface{}, error) {
+		_, err := usecase.todoRepository.GetTodo(ctx, entity.TodoID(e.ID))
+		if err != nil {
+			return entity.Todo{}, err
+		}
 
-	return e, nil
+		return entity.Todo{}, usecase.todoRepository.UpdateTodo(ctx, e)
+	})
+
+	return e, err
 }
 
 func (usecase TodoUsecase) DeleteTodo(e entity.TodoID) error {
 	ctx := context.Background()
-	err := usecase.todoRepository.DeleteTodo(ctx, e)
-	if err != nil {
-		return err
-	}
+	_, err := usecase.transaction.Do(ctx, func(ctx context.Context) (interface{}, error) {
+		return entity.Todo{}, usecase.todoRepository.DeleteTodo(ctx, e)
+	})
 
-	return nil
+	return err
 }
